74f1ceb7f98db4b440ca8cf49277cc910df050ac
Cloudwhile <3245567560@qq.com>
2025-03-11 21:24:05 +0800

    feact: 定义数据分析基础类结构

--- utils/data/index.js
+++ utils/data/index.js
@@ -1,1 +1,50 @@
-//something to do here
+const fs = require('fs');
+const path = require('path');
+
+class StudentData{
+    /**
+     * 
+     * @param {string} 学生姓名
+     * @param {string} 学生学号
+     * @param {string} 学生性别
+     * @param {number} 查询指定年份数据
+     */
+    constructor(name,stu_id,gender,year){
+        this.name = name;
+        this.stu_id = stu_id;
+        this.gender = gender;
+        this.year = year;
+        this.cache_origin_data = {};
+        this.cache_data = {};
+    }
+    async _getRemoteData(){
+        //...todo
+    }
+
+    async getFullData(){
+        //...todo
+    }
+
+    //...todo
+}
+
+class GradeData{
+    /**
+     * @param {string} 年级
+     */
+    constructor(grade_id){
+        this.grade_id = grade_id
+    }
+
+    async _getRemoteData(){
+        //...todo
+    }
+
+    async getFullData(){
+        //...todo
+    }
+
+    //...todo
+}
+
+module.exports = {StudentData, GradeData};
